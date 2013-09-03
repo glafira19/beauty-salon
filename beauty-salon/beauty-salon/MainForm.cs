b8301413f24b522d4f43e18684d3c0ab52007513
glafira19 <glafira-19@mail.ru>
2013-09-03 19:45:38 +0700

    начало работы с ролями пользователей

---
+++ beauty-salon/beauty-salon/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace beauty_salon
+{
+    public partial class MainForm : Form
+    {
+        public const string FORM_NAME = "Салон красоты \"ромашка\"";
+        private User _currentUser;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            _currentUser = new User("ProstoySmertniy");
+            UpdateFormName();
+        }
+
+        private void UpdateFormName()
+        {
+            Text = FORM_NAME + " | вы зашли как: "+_currentUser.GetName();
+        }
+    }
+}
---
+++ beauty-salon/beauty-salon/User.cs
@@ -0,0 +1,21 @@
+﻿namespace beauty_salon
+{
+    public class User
+    {
+        private string _name;
+        private Role _role;
+
+        public User(string name)
+        {
+            _role = Role.Guest;
+            _name = name;
+        }
+
+        public enum Role { Guest, User, Admin };
+
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+}
